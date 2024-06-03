5f6ed000e556177d18862ab6e4ea323e06210b17
kimi212 <Abirulkimi212@gmail.com>
2024-06-03 15:50:37 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template') #supplying template folder prarameter
 
 @app.route("/")
 def hello_world(): #define hello world
-    return 'Hello, Kimi!'
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
